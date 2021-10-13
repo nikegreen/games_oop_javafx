8fa6f9b152ca0cc081e033486757d047f0d4e561
nike z <nikegreen@yandex.ru>
2021-10-13 19:26:11 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,36 @@
 package ru.job4j.puzzle;
 
 public class Win {
+    public static boolean checkHorizontal(int[][] board, int row) {
+        for (int i:board[row]) {
+            if (i != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkVertical(int[][] board, int column) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+
+        for (int row = 0; row < board.length; row++) {
+            if (checkHorizontal(board, row)) {
+                return  true;
+            }
+        }
+        for (int col = 0; col < board[0].length; col++) {
+            if (checkVertical(board, col)) {
+                return  true;
+            }
+        }
+        return false;
     }
 }
